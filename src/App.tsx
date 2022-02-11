6eb84d8145d00857c4977470b4acac72397d1206
heroth <henning@codekollegen.de>
2022-02-11 09:39:24 +0100

    Added Router functionality to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { ContactList } from "./components/contacts/ContactList";
 import { ThemeSwitcher } from "./components/ThemeSwitcher";
 import { useAppSelector } from "./app/hooks";
 import { getTheme } from "./features/themeSwitcher/themeSwitcherSlice";
+import { Router } from "@reach/router";
 
 /* Assets */
 import logo from "./assets/img/logo.png";
 import logoWhite from "./assets/img/logo-white.png";
 import "./assets/css/App.css";
+import { ContactDetail } from "./components/contacts/ContactDetail";
+import { ContactForm } from "./components/contacts/ContactForm";
 
 function App() {
   const theme = useAppSelector(getTheme);
@@ -25,7 +28,20 @@ function App() {
     <div className="App" data-testid="app">
       <ThemeSwitcher />
       <img src={logoToShow} alt="Logo" title="Logo" className="App-logo" />
-      <ContactList />
+
+      <Router>
+        {/* Contact list for listing all available contacts */}
+        <ContactList path="/" />
+
+        {/* Contact detail Page for single contact */}
+        <ContactDetail path="/contact/:id" />
+
+        {/* Create a new contact */}
+        <ContactForm path="/create" />
+
+        {/* Edit an existing contact */}
+        <ContactForm path="/edit/:id" />
+      </Router>
     </div>
   );
 }
