818d6374f58e2580b6b70894a7c7238d015f91d1
heroth <henning@codekollegen.de>
2022-02-18 11:08:56 +0100

    Fixed tests

--- src/__test__/components/contacts/ContactDetail.test.tsx
+++ src/__test__/components/contacts/ContactDetail.test.tsx
@@ -2,12 +2,15 @@ import { render } from "@testing-library/react";
 import { store } from "../../../app/store";
 import { Provider } from "react-redux";
 import { ContactDetail } from "../../../components/contacts/ContactDetail";
+import { Router } from "@reach/router";
 
 describe("ContactDetail component", () => {
   it("should render correctly", async () => {
     const { getByTestId } = render(
       <Provider store={store}>
-        <ContactDetail />
+        <Router>
+          <ContactDetail path="/" />
+        </Router>
       </Provider>
     );
 
--- src/__test__/components/contacts/ContactForm.test.tsx
+++ src/__test__/components/contacts/ContactForm.test.tsx
@@ -2,12 +2,15 @@ import { render } from "@testing-library/react";
 import { ContactForm } from "../../../components/contacts/ContactForm";
 import { Provider } from "react-redux";
 import { store } from "../../../app/store";
+import { Router } from "@reach/router";
 
 describe("ContactForm component", () => {
   it("should render correctly", async () => {
     const { getByTestId } = render(
       <Provider store={store}>
-        <ContactForm />
+        <Router>
+          <ContactForm path="/" />
+        </Router>
       </Provider>
     );
 
